c3728944537b0e752ad1ed47e229cb4deaba58c4
MuriloRocha-01 <murilorochadelima.10@gmail.com>
2026-04-22 21:37:27 -0300

    atualizando baczada

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+const Tarefa = require('./models/Tarefa');
+
+// ROTA PRINCIPAL
+app.get("/", async (req, res) => {
+  try {
+    const tarefas = await Tarefa.find();
+    res.json(tarefas);
+  } catch (err) {
+    res.status(500).json({ erro: err.message });
+  }
+});
+
 const routes = require('./routes/routes');
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
-
-// MongoDB (CORRETO PARA RENDER)
 const mongoURL = process.env.MONGO_URL;
 
 mongoose.connect(mongoURL)
 .then(() => console.log("Database Connected"))
 .catch(err => console.log(err));
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server Started at ${PORT}`);
 });
