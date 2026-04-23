dd6e6a7c592b80f745bb48c98a35a0a6c1c3c3d3
MuriloRocha-01 <murilorochadelima.10@gmail.com>
2026-04-22 21:49:18 -0300

    atualizando baczada

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const express = require('express');
 const app = express();
-app.use((req, res, next) => {
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH,DELETE');
-res.header(
-"Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept"
-);
-next();
-});
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log(Server Started at ${PORT})
-})
+
 const routes = require('./routes/routes');
 app.use('/api', routes);
-var userArgs = process.argv.slice(2);
-var mongoURL = userArgs[0];
-var mongoose = require('mongoose');
-mongoose.connect(mongoURL);
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.on('error', (error) => {
-console.log(error)
-})
-db.once('connected', () => {
-console.log('Database Connected');
-})
+
+const PORT = process.env.PORT || 3000;
+
+// MongoDB (CORRETO PARA RENDER)
+const mongoURL = process.env.MONGO_URL;
+
+mongoose.connect(mongoURL)
+.then(() => console.log("Database Connected"))
+.catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`);
+});
